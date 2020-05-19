3ebbd72f5dfd10ea572ecf04fb81dbfb29ecc75a
Aditi <adkulkar@redhat.com>
2020-05-19 16:53:47 -0700

    Updated manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sqlite3
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helloworld_project.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello_work_demo.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
